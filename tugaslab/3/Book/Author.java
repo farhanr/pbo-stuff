74e9eaceaeeee880844a076ad8ffd815498ad80f
farhanr <fr150899@gmail.com>
2019-03-07 13:48:40 +0800

    ups forgot this one

---
+++ tugaslab/3/Book/Author.java
@@ -0,0 +1,18 @@
+class Author{
+    private String name,email;
+    private char gender;
+    Author(String name, String email, char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public char getGender(){
+        return this.gender;
+    }
+}
